f623017f8d999eae622df61e2c0cdb85a594327e
dev2820 <second28200@gmail.com>
2023-07-04 12:55:07 +0900

    refactor/ reduce 시작값을 0으로 고정

--- src/__tests__/sum.spec.ts
+++ src/__tests__/sum.spec.ts
@@ -1,6 +1,6 @@
 import { expect, it } from 'vitest';
 
-const sum = (...nums: number[]) => nums.reduce((acc, val) => acc + val);
+const sum = (...nums: number[]) => nums.reduce((acc, val) => acc + val, 0);
 
 it('should sum values', () => {
   expect(sum(1, 2, 3)).toBe(6);
