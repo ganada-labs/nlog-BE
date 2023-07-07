97db441b11816d8118ac37ad676fa73d5551fda1
dev2820 <second28200@gmail.com>
2023-07-07 16:59:34 +0900

    chore/ apidoc 설정 추가

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,18 @@ import Koa, { Context } from 'koa';
 
 export const app = new Koa();
 
+/**
+ * @api {get} /user/:id Request User information
+ *
+ * @apiVersion        0.1.0
+ * @apiName GetUser
+ * @apiGroup User
+ *
+ * @apiParam {Number} id Users unique ID.
+ *
+ * @apiSuccess {String} firstname Firstname of the User.
+ * @apiSuccess {String} lastname  Lastname of the User.
+ */
 app.use(async (ctx: Context) => {
   ctx.body = 'Hello World!';
 });
