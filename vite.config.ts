ea8e1d2427316ac59a6c76484957cffced97de4c
dev2820 <second28200@gmail.com>
2023-07-08 22:05:58 +0900

    chore/ vite alias 옵션 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,18 @@
 import { defineConfig } from 'vite';
 import { VitePluginNode } from 'vite-plugin-node';
+import { fileURLToPath } from 'url';
 
 export default defineConfig({
   server: {
     port: 3000,
+  },
+  resolve: {
+    alias: [
+      {
+        find: '@',
+        replacement: fileURLToPath(new URL('./src', import.meta.url)),
+      },
+    ],
   },
   plugins: [
     ...VitePluginNode({
