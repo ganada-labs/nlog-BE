925672f1a116220d1f39c39b06567bba18dfe3eb
dev2820 <second28200@gmail.com>
2023-07-08 21:35:47 +0900

    update/ read 함수가 일부 field만 반환할 수 있도록함

--- src/models/types.ts
+++ src/models/types.ts
@@ -1,6 +1,9 @@
 export interface CRUDable<T> {
   create: (data: T) => boolean | Promise<boolean>;
-  read: (query: Partial<T>) => T | Promise<T> | null;
+  read: (
+    query: Partial<T>,
+    select: Partial<T>
+  ) => null | Partial<T> | Promise<Partial<T> | null>;
   update: (query: Partial<T>, data: Partial<T>) => boolean | Promise<boolean>;
   remove: (query: Partial<T>) => boolean | Promise<boolean>;
 }
