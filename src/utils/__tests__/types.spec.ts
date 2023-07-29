eb69797ec840dafddc39840687e887db4286b265
dev2820 <second28200@gmail.com>
2023-07-29 13:31:55 +0900

    feat/ 타입 가드 추가(isNil, isEmail)

--- src/utils/__tests__/types.spec.ts
+++ src/utils/__tests__/types.spec.ts
@@ -1,9 +1,22 @@
 import { describe, expect, it } from 'vitest';
-import { isString, isNull } from '@/utils';
+import { isString, isNull, isUndefined, isNil, isEmail } from '@/utils';
 
 describe('types', () => {
   it('타입가드', () => {
     expect(isString('')).toBe(true);
     expect(isNull(null)).toBe(true);
+    expect(isUndefined(undefined)).toBe(true);
+
+    expect(isNil(null)).toBe(true);
+    expect(isNil(undefined)).toBe(true);
+  });
+
+  it('이메일 검증', () => {
+    expect(isEmail('johndoe@naver.com')).toBe(true);
+    expect(isEmail('johndoe@google.com')).toBe(true);
+    expect(isEmail('johndoe@some.ac.kr')).toBe(true);
+    expect(isEmail('@naver.com')).toBe(false);
+    expect(isEmail('johndoenaver.com')).toBe(false);
+    expect(isEmail('johndoe@naver')).toBe(false);
   });
 });
--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,4 +1,20 @@
+type Email = `${string}@${string}`;
+
 export const isString = (value: unknown): value is string =>
   typeof value === 'string';
 
 export const isNull = (value: unknown): value is null => value === null;
+
+export const isUndefined = (value: unknown): value is undefined =>
+  value === undefined;
+
+export const isNil = (value: unknown): value is null | undefined =>
+  isNull(value) || isUndefined(value);
+
+export const isEmail = (value: unknown): value is Email => {
+  if (!isString(value)) return false;
+
+  const EMAIL_REG =
+    /^[0-9a-zA-Z]+@[0-9a-zA-Z]+\.[a-zA-Z]{2,}(\.[a-zA-Z]{2,})?$/;
+  return EMAIL_REG.test(value);
+};
