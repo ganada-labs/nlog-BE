c7ce772ace02a8bdfc9856f3d350d24d22db2886
dev2820 <second28200@gmail.com>
2023-07-30 01:27:22 +0900

    refactor/ getBearerCredential에 대한 토큰 테스트 추가

--- src/utils/__tests__/token.spec.ts
+++ src/utils/__tests__/token.spec.ts
@@ -7,8 +7,9 @@ const setup = () => {
   };
   const accessToken = token.genAccessToken(content);
   const refreshToken = token.genRefreshToken(content);
+  const authorization = 'Bearer abc';
 
-  return { content, accessToken, refreshToken };
+  return { content, accessToken, refreshToken, authorization };
 };
 
 describe('token', () => {
@@ -47,5 +48,15 @@ describe('token', () => {
     expect(() => token.verify(refreshToken)).not.toThrowError();
     const decoded = token.verify(refreshToken);
     expect(isString(decoded) ? false : decoded.id).toBe(content.id);
+  });
+
+  it('authorization 헤더에서 토큰을 꺼내 반환할 수 있다.', () => {
+    const credential1 = token.getBearerCredential('Bearer abc');
+    const credential2 = token.getBearerCredential('B abc');
+    const credential3 = token.getBearerCredential('abc');
+
+    expect(credential1).toBe('abc');
+    expect(credential2).not.toBe('abc');
+    expect(credential3).not.toBe('abc');
   });
 });
