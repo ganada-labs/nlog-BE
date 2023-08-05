569ba376079e1d9a3808ef7b27d3a11654c4836c
dev2820 <second28200@gmail.com>
2023-08-06 01:17:05 +0900

    fix/ dotenv 관련 코드 제거

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
-import 'dotenv/config';
 import { app } from './dist/app.js';
 
-const { PORT } = process.env;
+const PORT = 3000;
 
 app.listen(PORT, () => {
-  // eslint-disable-next-line no-console
   console.log(`Listen on ${PORT}`);
 });
