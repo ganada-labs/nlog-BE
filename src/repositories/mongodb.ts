1c0dd378455b9ebec9da28ded554b5ece2a54650
dev2820 <second28200@gmail.com>
2023-08-06 16:12:07 +0900

    update/ mongodb 연결을 앱 시작과 동시에 시행

---
+++ src/repositories/mongodb.ts
@@ -0,0 +1,22 @@
+import mongoose from 'mongoose';
+
+const MONGO_HOST = import.meta.env.VITE_MONGO_HOST ?? 'localhost';
+const MONGO_PORT = import.meta.env.VITE_MONGO_PORT ?? '27019';
+const MONGO_URL = `mongodb://${MONGO_HOST}:${MONGO_PORT}/nlog`;
+
+export const connect = () => {
+  mongoose
+    .connect(MONGO_URL, {
+      serverSelectionTimeoutMS: 1000,
+    })
+    .then(
+      () => {
+        console.info('Mongo Connected!');
+      },
+      (err) => {
+        console.error(err.message);
+      }
+    );
+};
+
+export { Schema, model } from 'mongoose';
