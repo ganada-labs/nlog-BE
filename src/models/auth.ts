b3249c72742d4a52a2e2125c815f32113065bef5
dev2820 <second28200@gmail.com>
2023-08-07 11:22:48 +0900

    update/ redis 삭제 요청 생성

--- src/models/auth.ts
+++ src/models/auth.ts
@@ -14,7 +14,6 @@ const REDIS_PORT = import.meta.env.VITE_REDIS_PORT ?? '6379';
 const REDIS_URL = `redis://${REDIS_HOST}:${REDIS_PORT}`;
 
 const createKey = (query: TokenQuery) => `${query.email}`;
-let isConnected = false;
 
 const client = createClient({
   url: REDIS_URL,
@@ -22,7 +21,6 @@ const client = createClient({
 
 client.connect().then(
   () => {
-    isConnected = true;
     console.info('Redis Connected!');
   },
   (err) => {
@@ -32,10 +30,6 @@ client.connect().then(
 
 const set = async (query: TokenQuery, value: Token) => {
   try {
-    if (!isConnected) {
-      throw Error('DB is not connected');
-    }
-
     const key = createKey(query);
     await client.set(key, value);
 
@@ -48,12 +42,22 @@ const set = async (query: TokenQuery, value: Token) => {
   }
 };
 
+const remove = async (query: TokenQuery) => {
+  try {
+    const key = createKey(query);
+    await client.del(key);
+
+    return true;
+  } catch (err: unknown) {
+    if (err instanceof Error) {
+      console.error(err.message);
+    }
+    return false;
+  }
+};
+
 const get = async (query: TokenQuery) => {
   try {
-    if (!isConnected) {
-      throw Error('DB is not connected');
-    }
-
     const key = createKey(query);
     const result = await client.get(key);
 
@@ -69,4 +73,5 @@ const get = async (query: TokenQuery) => {
 export default {
   set,
   get,
+  remove,
 };
