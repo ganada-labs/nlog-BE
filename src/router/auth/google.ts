2388ee3090db5edf5540a2a2ce0ac6149c68a7c5
dev2820 <second28200@gmail.com>
2023-08-18 13:20:36 +0900

    refactor/ remove setRefreshTokenCookie

--- src/router/auth/google.ts
+++ src/router/auth/google.ts
@@ -2,9 +2,10 @@ import Router from '@koa/router';
 import passport from '@/middlewares/passport';
 import { normGoogleUser } from '@/middlewares/normGoogleUser';
 import { saveRefreshToken } from '@/middlewares/saveRefreshToken';
-import { setRefreshTokenCookie } from '@/middlewares/setRefreshTokenCookie';
+import * as Auth from '@/services/auth';
 
 const OAUTH_REDIRECT_URL = import.meta.env.VITE_OAUTH_REDIRECT_URL;
+const DOMAIN = import.meta.env.VITE_DOMAIN;
 
 const GoogleAuth = new Router();
 
@@ -26,7 +27,19 @@ GoogleAuth.get(
   }),
   normGoogleUser,
   saveRefreshToken,
-  setRefreshTokenCookie
+  async (ctx) => {
+    const { refreshToken } = ctx;
+
+    ctx.cookies.set('refresh_token', refreshToken, {
+      httpOnly: true,
+      domain: DOMAIN,
+      maxAge: Auth.REFRESH_TOKEN_EXPIRES_IN * 1000,
+      sameSite: 'strict',
+      path: '/',
+    });
+
+    ctx.redirect(OAUTH_REDIRECT_URL);
+  }
 );
 
 GoogleAuth.get('/callback/failure', (ctx) => {
