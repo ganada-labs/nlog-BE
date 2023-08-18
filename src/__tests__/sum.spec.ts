5cfde77f12cac0890250228ce44a246ab23a3190
dev2820 <second28200@gmail.com>
2023-08-18 14:17:40 +0900

    refactor/ 사용되지 않는 테스트 제거

--- src/__tests__/sum.spec.ts
+++
@@ -1,7 +0,0 @@
-import { expect, it } from 'vitest';
-
-const sum = (...nums: number[]) => nums.reduce((acc, val) => acc + val, 0);
-
-it('should sum values', () => {
-  expect(sum(1, 2, 3)).toBe(6);
-});
