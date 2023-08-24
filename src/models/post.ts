1296c1f63c7f7d1f3b27cb51156284a43f84213b
dev2820 <second28200@gmail.com>
2023-08-24 18:28:43 +0900

    refactor/ post model 생성

---
+++ src/models/post.ts
@@ -0,0 +1,102 @@
+import { Schema, model } from '@/infrastructures/mongodb.ts';
+import type { CRUDable, Create, Read, Update, Remove } from './types.ts';
+
+export interface MetaSchema {
+  author: string;
+  createdAt?: Date;
+  modifiedAt?: Date;
+}
+
+export interface PostSchema {
+  id: string;
+  title: string;
+  contents?: object[];
+  meta: MetaSchema;
+}
+
+const metaSchema = new Schema<MetaSchema>({
+  author: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now },
+  modifiedAt: { type: Date, default: Date.now },
+});
+
+const postSchema = new Schema<PostSchema>({
+  id: { type: String, required: true },
+  title: { type: String, required: true },
+  contents: { type: Array, default: [] },
+  meta: metaSchema,
+});
+
+const PostModel = model<PostSchema>('Post', postSchema);
+
+const create: Create<PostSchema> = async (data: PostSchema) => {
+  try {
+    const isExist = await PostModel.exists({ id: data.id });
+    if (isExist) {
+      throw Error(`key: ${data.id} is already exist`);
+    }
+
+    await new PostModel({
+      ...data,
+    }).save();
+
+    return true;
+  } catch (err: unknown) {
+    if (err instanceof Error) {
+      console.error(err.message);
+    }
+    return false;
+  }
+};
+
+const read: Read<PostSchema> = async (
+  query: Partial<PostSchema>,
+  select?: Partial<PostSchema>
+) => {
+  try {
+    const result = await PostModel.findOne(query, select);
+
+    return result;
+  } catch (err: unknown) {
+    if (err instanceof Error) {
+      console.error(err.message);
+    }
+    return null;
+  }
+};
+
+const update: Update<PostSchema> = async (
+  query: Partial<PostSchema>,
+  data: Partial<PostSchema>
+) => {
+  try {
+    await PostModel.updateOne(query, data);
+
+    return true;
+  } catch (err: unknown) {
+    if (err instanceof Error) {
+      console.error(err.message);
+    }
+    return false;
+  }
+};
+
+const remove: Remove<PostSchema> = async (query: Partial<PostSchema>) => {
+  try {
+    await PostModel.deleteOne(query);
+
+    return true;
+  } catch (err: unknown) {
+    if (err instanceof Error) {
+      console.error(err.message);
+    }
+    return false;
+  }
+};
+
+export default {
+  create,
+  read,
+  update,
+  remove,
+} satisfies CRUDable<PostSchema>;
