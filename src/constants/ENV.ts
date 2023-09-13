a48561a0f3cfad7ab2f6ff6f2973d78bc12084ad
dev2820 <second28200@gmail.com>
2023-09-13 22:09:33 +0900

    update : development 환경 변수 생성

--- src/constants/ENV.ts
+++ src/constants/ENV.ts
@@ -1,1 +1,5 @@
-export const { MONGO_HOST, MONGO_PORT, REDIS_HOST, REDIS_PORT } = process.env;
+export const MONGO_HOST = import.meta.env.VITE_MONGO_HOST ?? 'localhost';
+export const MONGO_PORT = import.meta.env.VITE_MONGO_PORT ?? '27019';
+export const MONGO_DBNAME = import.meta.env.VITE_MONGO_DBNAME;
+export const REDIS_HOST = import.meta.env.VITE_REDIS_HOST;
+export const REDIS_PORT = import.meta.env.VITE_REDIS_PORT;
