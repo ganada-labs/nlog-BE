31b9015154b6854882bd6a29d72e8d623572e4ba
dev2820 <second28200@gmail.com>
2023-09-15 00:28:18 +0900

    fix : 개발서버에서 설정하는 쿠키의 도메인은 localhost가 되도록 설정

--- src/constants/ENV.ts
+++ src/constants/ENV.ts
@@ -4,3 +4,5 @@ export const MONGO_DBNAME = import.meta.env.VITE_MONGO_DBNAME;
 export const REDIS_HOST = import.meta.env.VITE_REDIS_HOST ?? 'localhost';
 export const REDIS_PORT = import.meta.env.VITE_REDIS_PORT ?? '6379';
 export const REDIS_PREFIX = import.meta.env.VITE_REDIS_PREFIX ?? '';
+export const OAUTH_REDIRECT_URL = import.meta.env.VITE_OAUTH_REDIRECT_URL;
+export const DOMAIN = import.meta.env.VITE_DOMAIN;
